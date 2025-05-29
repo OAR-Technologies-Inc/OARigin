f35ca6cc9df51f3f0bd532a2eea814fbcb81c74f
southerncory <southerncory@gmail.com>
2025-05-29 13:30:55 -0400

    Updated 20250529142827_gentle_gate.sql

--- supabase/migrations/20250529142827_gentle_gate.sql
+++ supabase/migrations/20250529142827_gentle_gate.sql
@@ -281,16 +281,5 @@ CREATE TRIGGER update_user_stats_updated_at
   FOR EACH ROW
   EXECUTE FUNCTION public.update_updated_at_column();
 
-DO $$ 
-BEGIN
-  IF NOT EXISTS (
-    SELECT 1 
-    FROM pg_trigger 
-    WHERE tgname = 'on_auth_user_created'
-  ) THEN
-    CREATE TRIGGER on_auth_user_created
-    AFTER INSERT ON auth.users
-    FOR EACH ROW
-    EXECUTE FUNCTION public.handle_new_user();
   END IF;
 END $$;
