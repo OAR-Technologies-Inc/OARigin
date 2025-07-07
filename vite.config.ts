4134b8c0bf801c869d64b4e9199456d13e68cec3
The Treasurer <southerncory@gmail.com>
2025-07-07 22:29:29 +0000

    Another fucking vite update 	modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,23 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import nodePolyfills from 'vite-plugin-node-polyfills'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    nodePolyfills(),
-  ],
+  plugins: [react(), nodePolyfills()],
   define: {
     global: 'globalThis',
+    'process.env': {}, // ensures process is defined
+  },
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+      stream: 'stream-browserify',
+      assert: 'assert',
+      buffer: 'buffer',
+    },
   },
   optimizeDeps: {
-    include: ['crypto'],
+    include: ['crypto', 'stream', 'assert', 'buffer'],
   },
 })
+
