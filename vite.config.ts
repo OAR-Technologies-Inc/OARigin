fce93adf9b0c7052168a1a8bd5152ef78557fe94
The Treasurer <southerncory@gmail.com>
2025-07-07 22:36:07 +0000

    once again Vite update 	modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import nodePolyfills from 'vite-plugin-node-polyfills'
+import { webcrypto } from 'node:crypto'
 
+if (!globalThis.crypto) {
+  globalThis.crypto = webcrypto as unknown as Crypto
+}
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), nodePolyfills()],
